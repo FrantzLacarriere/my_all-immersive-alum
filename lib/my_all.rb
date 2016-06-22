3ad8c7de05f7527c19763418c96fceda3fc37658
FrantzLacarriere <frantz.lacarriere@gmail.com>
2016-06-22 14:27:01 -0400

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  val = true
+  while i < collection.size
+    if !yield(collection[i])
+      val = false
+      break
+    end
+    i += 1
+  end
+  val
 end
